cced1bf79b81df805a1002a8b59507124f79768d
taeck <taeck007@gmail.com>
2021-06-06 19:34:06 +0900

    Move button, select component to component/common

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as AppBootstrap } from "./app-bootstrap/app-bootstrap";
+export { default as AppBootstrap } from "./common/app-bootstrap/app-bootstrap";
 export { default as Button_1 } from "./common/button_1/button1";
-export { default as Select_1 } from "./select_1/select_1";
-export { default as Text } from './text/text'
+export { default as Select_1 } from "./common/select_1/select_1";
+export { default as Text } from './common/text/text'
 export { default as Background } from './common/background/background'
-export { default as TextInput } from './text-input/text-input'
+export { default as TextInput } from './common/text-input/text-input'
