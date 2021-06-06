bbd7e73f29a68d38e7b9670fd3cefe9df0546606
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-06-06 19:07:11 +0900

    모듈정리

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as AppBootstrap } from "./app-bootstrap/app-bootstrap";
-export { default as Button_1 } from "./button_1/button1";
+export { default as Button_1 } from "./common/button_1/button1";
 export { default as Select_1 } from "./select_1/select_1";
 export { default as Text } from './text/text'
-export { default as Background } from './background/background'
+export { default as Background } from './common/background/background'
 export { default as TextInput } from './text-input/text-input'
