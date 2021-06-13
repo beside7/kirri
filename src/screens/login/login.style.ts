b6015d5961f566a9c3e4088e710acd1001d2e2db
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-06-13 14:35:12 +0900

    .

---
+++ src/screens/login/login.style.ts
@@ -0,0 +1,43 @@
+import { globalStyles } from "@utils";
+import { StyleSheet, Dimensions } from 'react-native';
+
+const ScreenHeight = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+    container: {
+        ...globalStyles.container
+    },
+    content: {
+        width: "100%",
+        height: ScreenHeight,
+        paddingTop: 40,
+        paddingBottom: 20,
+        paddingLeft: 30,
+        paddingRight: 30,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 42,
+        marginBottom: 80
+    },
+    subTitle: {
+        alignItems: "center",
+        marginBottom: 80
+    },
+    subTitleText: {
+        fontSize: 18,
+        color: "#707070",
+        marginTop: 3,
+        marginBottom: 3
+    },
+    button: {
+        marginBottom: 20
+    },
+    message: {
+        fontSize: 12,
+        color: "#a4a4a4",
+    },
+});
+
+export default styles;
