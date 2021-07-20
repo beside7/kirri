3b03f618915bf5c73377dbfedb938007905d0ae5
je.kim <gadian88@gmail.com>
2021-07-20 15:15:24 +0900

    설정

--- src/screens/setting/index.tsx
+++ src/screens/setting/index.tsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { Switch } from 'react-native-paper';
 
 import {
     Container, 
     ProfileContainer, 
     ProfileImage,
     SelectProfileContainer,
-    SelectProfileImage
+    SelectProfileImage,
+    Description,
+    TextInput,
+    Placeholder,
+    SettingContainer,
+    SettingColumn,
+    SettingColumnTitle,
+    SwitchContainer,
+    Icon,
+    WithdrawalContainer,
+    WithdrawalText
 } from "./style";
 
 import { Background, Header,  } from "@components";
@@ -48,7 +59,75 @@ export default function Setting() {
                             />
                         </TouchableOpacity>
                     </SelectProfileContainer>
+                    <Description>
+                        한글, 영문, 숫자를 사용해 멋진 닉네임을 만들어주세요!
+                    </Description>
+                    <View>
+                        <TextInput />
+                        <Placeholder>끼리</Placeholder>
+                    </View>
                 </ProfileContainer>
+                <SettingContainer>
+                    <SettingColumn>
+                        <SwitchContainer>
+                            <Icon 
+                            source={require("@assets/images/setting_notice_setting.png")}
+                            />
+                        </SwitchContainer>
+                        <SettingColumnTitle>알림 설정</SettingColumnTitle>
+                        <SwitchContainer style={{ alignItems : 'flex-end'}}>
+                            <Switch />
+                        </SwitchContainer>
+                    </SettingColumn>
+                </SettingContainer>
+                <SettingContainer>
+                    <SettingColumn>
+                        <SwitchContainer>
+                            <Icon 
+                            source={require("@assets/images/setting_personal_info.png")}
+                            />
+                        </SwitchContainer>
+                        <SettingColumnTitle>개인정보 처리 방침</SettingColumnTitle>
+                        <SwitchContainer style={{ alignItems : 'flex-end', paddingHorizontal:10 }}>
+                            <Icon 
+                                source={require("@assets/images/setting_next_normal.png")}
+                            />
+                        </SwitchContainer>
+                    </SettingColumn>
+                </SettingContainer>
+                <SettingContainer>
+                    <SettingColumn>
+                        <SwitchContainer>
+                            <Icon 
+                            source={require("@assets/images/setting_version.png")}
+                            />
+                        </SwitchContainer>
+                        <SettingColumnTitle>버전정보</SettingColumnTitle>
+                        <SwitchContainer style={{ alignItems : 'flex-end', paddingHorizontal:1 }}>
+                            <Text>V1.0.5</Text>
+                        </SwitchContainer>
+                    </SettingColumn>
+                </SettingContainer>
+                <SettingContainer>
+                    <SettingColumn>
+                        <SwitchContainer>
+                            <Icon 
+                            source={require("@assets/images/setting_logout.png")}
+                            />
+                        </SwitchContainer>
+                        <SettingColumnTitle>로그아웃</SettingColumnTitle>
+                        <SwitchContainer style={{ alignItems : 'flex-end', paddingHorizontal:10 }}>
+                            <Icon 
+                                source={require("@assets/images/setting_next_normal.png")}
+                            />
+                        </SwitchContainer>
+                    </SettingColumn>
+                </SettingContainer>
+                <WithdrawalContainer>
+                    <WithdrawalText>
+                        아쉽지만 탈퇴 할래요.
+                    </WithdrawalText>
+                </WithdrawalContainer>
             </Container>
         </Background>
     )
--- src/screens/setting/style.ts
+++ src/screens/setting/style.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
     flex: 1;
 `
 
 export const ProfileContainer = styled.View`
-    height: 341;
+    height: 441;
     border-bottom-width: 8;
     border-color: #f7f7f7;
     align-items: center;
-    justify-content: center;
+    padding-top: 40;
 `
 
 export const ProfileImage = styled.Image`
@@ -28,4 +28,80 @@ export const SelectProfileImage = styled.Image`
     height: 54;
     margin-right: 19;
     margin-left: 19;
+`
+
+export const Description = styled.Text`
+    margin-top: 70;
+    color: #17171c;
+    font-size: 12;
+    font-family: 'SpoqaHanSansNeo-Medium';
+`
+
+export const TextInput = styled.TextInput`
+    padding-left: 12;
+    margin-top: 8;
+    margin-bottom: 60;
+    border-width: 1.5;
+    border-color: #d1d1de;
+    width: 307;
+    height: 40;
+    border-radius: 10;
+`
+
+export const Placeholder = styled.Text`
+    position: absolute;
+    top: 20;
+    right: 18;
+    font-size: 12;
+    color: #6f6f7e;
+`
+
+export const SettingContainer = styled.View`
+`
+
+export const SettingColumn = styled.View`
+    /* width: 375; */
+    height: 60;
+    flex-direction: row;
+    padding-left: 34;
+    padding-right: 34;
+    align-self: center;
+`
+
+export const SettingColumnTitle = styled.Text`
+    width: 259;
+    height: 60;
+    text-align: left;
+    padding-left: 12;
+    padding-top: 21;
+    padding-bottom: 21;
+    font-size: 14;
+    font-family: 'SpoqaHanSansNeo-Medium';
+    color: #17171c;
+`
+
+export const Icon = styled.Image`
+    width: 24;
+    height: 24;
+`
+
+export const SwitchContainer = styled.View`
+    width: 50;
+    justify-content: center;
+    height: 60;
+`
+
+export const WithdrawalContainer = styled.View`
+    height: 100;
+    background-color: #f7f7f7;
+    padding-left: 70;
+    padding-right: 70;
+    padding-top: 12;
+`
+
+export const WithdrawalText = styled.Text`
+    color: #6f6f7e;
+    text-decoration: underline;
+    font-family: 'SpoqaHanSansNeo-Regular';
+    font-size: 14;
 `
