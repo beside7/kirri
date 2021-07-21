8db47de0f7ba626eee7a097970e10bfec37dd4e5
je.kim <gadian88@gmail.com>
2021-07-21 10:23:05 +0900

    header 수정

--- src/screens/friend-main/index.tsx
+++ src/screens/friend-main/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { View, SafeAreaView } from 'react-native'
 import styles from './friend-main.style'
 import AppNavigator from './tab-navigator';
-import Header_1 from './header_1/header_1'
 import { Background, Header } from "@components";
 
 
