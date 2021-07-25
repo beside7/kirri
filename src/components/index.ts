358401cdcc39e6d655272c0a26672d980b76c4b9
taeck <taeck007@gmail.com>
2021-07-25 17:43:35 +0900

    Remove select component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as AppBootstrap } from "./common/app-bootstrap/app-bootstrap";
-export { default as Select_1 } from "./common/select_1/select_1";
+// export { default as Select_1 } from "./common/select_1/select_1";
 export { default as Text_1 } from './common/text_1/text_1'
 export { default as Text_2 } from './common/text_2/text_2'
 export { default as Text } from './common/text_2/text_2'
