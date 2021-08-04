2fac2202c8a8456d0c964d4e4c5ab6e7d304c352
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-08-04 16:31:17 +0100

    style 변경

--- src/screens/friend-main/style.ts
+++ src/screens/friend-main/style.ts
@@ -1,26 +1,26 @@
 import styled from "styled-components/native";
 
-export const TextInput = styled.TextInput`
-    border-color: #d1d1de;
-    border-width: 1;
-    border-radius: 5;
-    height: 40;
-    padding-top: 11;
-    padding-bottom: 11;
-    padding-left: 12;
-    padding-right: 12;
-`
+export const TextInput = styled.TextInput({
+    borderColor: "#d1d1de",
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 40,
+    paddingTop: 11,
+    paddingBottom: 11,
+    paddingLeft: 12,
+    paddingRight: 12,
+})
 
 export const Icon = styled.Image({
     width: 24,
     height: 24,
 })
 
-export const Button = styled.TouchableOpacity`
-    position: absolute;
-    top: 8;
-    right: 8;
-`
+export const Button = styled.TouchableOpacity({
+    position: "absolute",
+    top: 8,
+    right: 8,
+})
 
 /**
  * 친구내보내기 부분
