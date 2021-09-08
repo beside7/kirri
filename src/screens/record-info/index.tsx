255490e2e0ec61a73a5763016a59489abcf23d8a
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-09-08 16:30:50 +0100

    KIRI-112 [다이어리정보] 슬라이드 업 할 시 간지페이지 위로 날라가고 아래에서 다이어리 홈 올라오기

--- src/screens/record-info/index.tsx
+++ src/screens/record-info/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import Background from "./background";
+import React, {ReactElement, ReactNode, useState} from 'react'
+import {View, Animated, PanResponder, Image,Dimensions} from 'react-native'
 import Color from './color'
 
-import { Container, HeaderImage, Title, Content, Footer,Author , Button, Icon , ButtonText } from "./style";
+import { Container, HeaderImage, Title, Content, Footer,Author , Button, Icon , ButtonText, ScrollDownButton } from "./style";
 
 import { StackNavigatorParams } from "@config/navigator";
 import { StackNavigationProp } from "@react-navigation/stack";
@@ -13,11 +12,16 @@ import { CoverBigImages, CoverColorTypes, CoverColor } from "@utils";
 
 import dateFormat from 'dateformat'
 import { stringToDatetime } from '@utils'
+import Constants from "expo-constants";
 
 type RecordInfoProps = {
     navigation: StackNavigationProp<StackNavigatorParams, "RecordInfo">;
     route: RouteProp<StackNavigatorParams, "RecordInfo">;
 }
+
+type BackgroundProps = {
+    children: ReactNode;
+};
 
 export default function RecordInfo({navigation , route} : RecordInfoProps) {
     const diary = route.params.diary;
@@ -25,9 +29,72 @@ export default function RecordInfo({navigation , route} : RecordInfoProps) {
     const [ coverType , coverId ] = icon.split(":")
 
 
-
     const { nickname } = members[0];
-    
+    const [pan, setPan] = useState(new Animated.ValueXY());
+
+    /**
+     * 애니메이션 이벤트 및 핸들링
+     */
+    const panResponder =  PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => true,
+        onPanResponderMove: Animated.event([null,{
+            // dx  : pan.x,
+            dy  : pan.y
+        }]),
+        onPanResponderRelease: (e, gesture) => {
+            /**
+             * 화면 이동후 제자리로 돌아감
+             */
+            Animated.spring(
+                pan,
+                {useNativeDriver: false, toValue:{x:0,y:0}}
+            ).start();
+            /**
+             * 만약 하단으로 화면 드래그시 리스트 화면으로 넘어감
+             */
+            if(gesture.dy < 0){
+                navigation.navigate("RecordList", { diary: diary })
+            }
+        }
+    })
+
+    /**
+     * 백그라운트 이미지
+     * @param children
+     * @constructor
+     */
+    const Background = ({ children } : BackgroundProps) : ReactElement => {
+        return (
+            <Animated.View
+                {...panResponder.panHandlers}
+                style={
+                    [
+                        pan.getLayout(),
+                        {
+                            flex: 1,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            left: 0,
+                        }
+                    ]
+                }
+            >
+                <Image
+                    source={require("@assets/images/record/diary_overview_bgimg_color_01.png")}
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "absolute",
+                        left: 0,
+                        right: 0,
+                        top: Constants.statusBarHeight,
+                        bottom: 0
+                    }}
+                />
+                {children}
+            </Animated.View>
+        )
+    }
 
     return (
         <Background>
@@ -57,7 +124,7 @@ export default function RecordInfo({navigation , route} : RecordInfoProps) {
 
             <Button
                 onPress={()=>{
-                    navigation.navigate("RecordList", { diary: diary })
+                    navigation.navigate("RecordInput", { diary: diary })
                 }}
             >
                 <Icon 
@@ -65,6 +132,19 @@ export default function RecordInfo({navigation , route} : RecordInfoProps) {
                 />
                 <ButtonText>오늘의 너를 들려줘 :)</ButtonText>
             </Button>
+
+            <View
+                style={{
+                    position: 'absolute',
+                    bottom: 38
+                }}
+            >
+                <ScrollDownButton
+                    source={require("@assets/images/various_collapse_on_normal.png")}
+                />
+            </View>
         </Background>
     )
 }
+
+const Window = Dimensions.get('window');
--- src/screens/record-info/style.ts
+++ src/screens/record-info/style.ts
@@ -1,59 +1,64 @@
 import styled from "styled-components/native";
 
 
-export const Container = styled.View`
-    width: 219;
-    height: 249;
-    background-color: #fff;
-    border-radius: 10;
-`
+export const Container = styled.View({
+    width: 219,
+    height: 249,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+})
 
 export const HeaderImage = styled.Image`
     
 `
-export const Content = styled.View`
-    padding-top: 24;
-    padding-left: 28;
-    padding-right: 28;
-    padding-bottom: 20;
-`
+export const Content = styled.View({
+    paddingTop: 24,
+    paddingLeft: 28,
+    paddingRight: 28,
+    paddingBottom: 20,
+})
 
-export const Footer = styled.View`
-    margin-top: 55;
-    justify-content: flex-end;
-`
+export const Footer = styled.View({
+    marginTop: 55,
+    justifyContent: "flex-end",
+})
 
-export const Title = styled.Text`
-    text-align: center;
-    font-size: 20;
-    font-family: "SpoqaHanSansNeo-Medium";
-`
+export const Title = styled.Text({
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "SpoqaHanSansNeo-Medium",
+})
 
-export const Author = styled.Text`
-    color: #6f6f7e;
-    font-family: 'SpoqaHanSansNeo-Regular';
-    font-size: 12;
-`
+export const Author = styled.Text({
+    color: '#6f6f7e',
+    fontFamily: 'SpoqaHanSansNeo-Regular',
+    fontSize: 12,
+})
 
-export const Button = styled.TouchableOpacity`
-    width: 220;
-    height: 50;
-    align-items: center;
-    justify-content: center;
-    background-color: #ffdd1f;
-    flex-direction: row;
-    border-radius: 10;
-    margin-top: 40;
-`
+export const Button = styled.TouchableOpacity({
+    width: 220,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffdd1f',
+    flexDirection: 'row',
+    borderRadius: 10,
+    marginTop: 40,
+})
 
-export const Icon = styled.Image`
-    width: 24;
-    height: 24;
-    margin-right: 4;
-`
+export const Icon = styled.Image({
+    width: 24,
+    height: 24,
+    marginRight: 4,
+})
 
-export const ButtonText = styled.Text`
-    font-size: 14;
-    font-family: 'SpoqaHanSansNeo-Medium';
-    color: #17171c;
-`
+export const ButtonText = styled.Text({
+    fontSize: 14,
+    fontFamily: 'SpoqaHanSansNeo-Medium',
+    color: '#17171c',
+})
+
+export const ScrollDownButton = styled.Image({
+    width: 24,
+    height: 24
+})
