620895101c979c867838df8fc8a6237f5c5ac787
DESKTOP-9TOEK4L\gadia <gadian88@gmail.com>
2021-09-11 22:53:59 +0900

    KIRI-166 [다이어리>나가기] 나가기 기능 구현 필요

---
+++ src/screens/record-list/leave-confirm.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Image } from 'react-native'
+import { Confirm, Text_2 } from "@components";
+import styles from "./style";
+
+type LeaveConfirmProps = {
+    visible : boolean
+    onClose : () => void
+    onConfirm: () => void
+    confirm?: string
+    close?: string
+    nickName?: string | null
+}
+
+
+/**
+ * 떠나기 팝업
+ * @param props
+ * @constructor
+ */
+export function LeaveConfirm(props : LeaveConfirmProps) {
+    return (
+        <Confirm
+            {...props}
+            content={
+                <>
+                    <View style={styles.modalImages}>
+                        <Image
+                            source={require("@assets/images/popup_diary_delete_bgimg.png")}
+                            style={styles.modalDeleteIcon}
+                        />
+                        <Text_2 style={styles.modalTitleStyle}>작성한 기록이 모두 사라져요.</Text_2>
+                        <Text_2 style={styles.modalTitleStyle}>다이어리를 정말 떠나시겠어요?</Text_2>
+                    </View>
+                </>
+            }
+        />
+    )
+}
+
